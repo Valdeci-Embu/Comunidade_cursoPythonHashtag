718294c147a6addcbd86c10ae80f174ac4822304
Valdeci <valdecie@gmail.com>
2023-12-12 17:32:30 -0300

    Update __init__.py

--- comunidade_impressionadora/__init__.py
+++ comunidade_impressionadora/__init__.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'dec2473a6bca9f423cbc3d27573a6a80'
 
-if os.getenv('DATABASE_PRIVATE_URL'):
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_PRIVATE_URL')
+if os.getenv('DATABASE_URL'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
@@ -23,7 +23,7 @@ login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
 
 from comunidade_impressionadora import models
-engine = sqlalchemy.create_engine(app.config['SQLAlCHEMY_DATABASE_URI'])
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 inspector = sqlalchemy.inspect(engine)
 if not inspector.has_table("usuario"):
     with app.app_context():
